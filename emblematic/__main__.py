f606315eea518b2d6746a1c51c7b4fc1d70182bb
Stefano Pigozzi <me@steffo.eu>
2023-03-08 00:41:42 +0100

    Start doing something

--- emblematic/__main__.py
+++ emblematic/__main__.py
@@ -1,7 +1,37 @@
-# If you are building an **application**, use this file to run code!
+import click
+import bs4
 
-def main():
-    pass
+
+@click.command()
+@click.option(
+    "-o", "--output-file", "output_file",
+    help="File to output the icon to.", 
+    required=True,
+    type=click.File(mode="w"),
+)
+@click.option(
+    "-b", "--background-file", "background_file",
+    help="File to use as the background of the icon.",
+    required=True,
+    type=click.File(mode="r"),
+)
+@click.option(
+    "-f", "--foreground-file", "foreground_file",
+    help="File to use as the foreground of the icon.",
+    required=True,
+    type=click.File(mode="r"),
+)
+def main(output_file, background_file, foreground_file):
+    background_soup = bs4.BeautifulSoup(background_file, "lxml-xml")
+    foreground_soup = bs4.BeautifulSoup(foreground_file, "lxml-xml")
+
+    foreground_tag = foreground_soup.path
+    foreground_scaled_tag = bs4.BeautifulSoup("""<g id="icon" transform="matrix(2,0,0,2,-4.3209502e-5,512)"></g>""", "lxml-xml")
+    foreground_scaled_tag.g.append(foreground_tag)
+
+    background_soup.svg.append(foreground_scaled_tag)
+
+    output_file.write(background_soup.prettify())
 
 
 if __name__ == "__main__":
